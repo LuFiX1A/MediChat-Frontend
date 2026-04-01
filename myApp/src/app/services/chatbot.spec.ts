a296858d455098dad161a11fd8671969c558070a
FerL719 <luis.fernandorm.43@gmail.com>
2026-04-01 00:03:29 -0600

    Ajustes finales
    Servicios funcionales y logica final pulida

--- myApp/src/app/services/chatbot.spec.ts
+++ myApp/src/app/services/chatbot.spec.ts
@@ -1,13 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ChatbotService } from './chatbot'; // Cambiado de Chatbot a ChatbotService
 
-import { Chatbot } from './chatbot';
-
-describe('Chatbot', () => {
-  let service: Chatbot;
+describe('ChatbotService', () => {
+  let service: ChatbotService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(Chatbot);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Añadido para que no falle por el HttpClient
+      providers: [ChatbotService]
+    });
+    service = TestBed.inject(ChatbotService);
   });
 
   it('should be created', () => {
